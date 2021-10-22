efdef6911399fee8bae31d2c7ab76af5968739c1
banatehrani <banatehrani@gmail.com>
2021-10-22 10:30:38 -0400

    file reports.py added

--- main.py
+++ main.py
@@ -1,49 +1,6 @@
-import webbrowser
-
-from fpdf import FPDF
-
 from room import Bill, Roommate
 
-
-class PdfReport:
-    """
-    Creates a .pdf file that contains information about
-    the roommates such as their names, their due amounts
-    and the period of the bill.
-    """
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def generate(self, roommate1, roommate2, bill):
-        pdf = FPDF(orientation='P', unit='pt', format='A4')
-        pdf.add_page()
-
-        # Add image
-        pdf.image("house.png", w=30, h=30)
-
-        # Insert title
-        pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt="Roommates Bill", border=0, align="C", ln=1)
-
-        # Insert period label and value
-        pdf.set_font(family="Times", size=14, style='B')
-        pdf.cell(w=100, h=40, txt="Period:", border=0)
-        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
-
-        # Insert name and due amount of the first roommate
-        pdf.set_font(family="Times", size=12)
-        pdf.cell(w=100, h=25, txt=roommate1.name, border=0)
-        pdf.cell(w=150, h=25, txt=str(roommate1.pays(bill, roommate2)), border=0, ln=1)
-
-        # Insert name and due amount of the second roommate
-        pdf.cell(w=100, h=25, txt=roommate2.name, border=0)
-        pdf.cell(w=150, h=25, txt=str(roommate2.pays(bill, roommate1)), border=0)
-
-        pdf.output(self.filename)
-
-        webbrowser.open(self.filename)  # Convert it to absolute path for Mac and Linux
-
+from reports import PdfReport
 
 amount = float(input("Please enter the bill amount: "))
 period = input("What is the bill period? e.g December 2020:  ")
